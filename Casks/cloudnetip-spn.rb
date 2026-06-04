0e3006499c73b621994510892253db9c64c9de57
Oxmix <oxmix@me.com>
2026-06-04 11:08:44 +0300

    cloudnetip-spn 0.6.0

--- Casks/cloudnetip-spn.rb
+++ Casks/cloudnetip-spn.rb
@@ -1,13 +1,13 @@
 cask "cloudnetip-spn" do
-  version "0.5.0"
-  sha256 "f5fd0578acad153277719c3def5c20b4660045d20ee7f11b56fa6a529b4ee66b"
+  version "0.6.0"
+  sha256 "5b466c55ccc32653de0c80cb6c45be8270fe901cd1709ba491ba75b3ce695ca8"
 
   url "https://github.com/cloudnetip/netip-spn/releases/download/v#{version}/Cloudnetip-SPN-#{version}.zip"
   name "Cloudnetip SPN"
   desc "Menubar app for the Cloudnetip Shared Private Network"
   homepage "https://github.com/cloudnetip/netip-spn"
 
-  depends_on formula: "cloudnetip/spn/cloudnetip-spn"
+  depends_on formula: "cloudnetip/tap/cloudnetip-spn"
   depends_on macos: ">= :ventura"
 
   app "Cloudnetip SPN.app"
